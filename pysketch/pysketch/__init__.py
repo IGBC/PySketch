bb60fb9d1d69da1a2da6d07f9b56213520a7742f
IGBC <fakespamtrap@gmail.com>
2016-05-19 02:02:19 +0200

    Built interpreter + fixed bugs

--- pysketch/pysketch/__init__.py
+++ pysketch/pysketch/__init__.py
@@ -1,3 +1,26 @@
-import runner
+from .runner import SketchRunner as SketchRunner
 
-__all__ = ["runner"]
+
+def interpreter(args):
+    if len(args) == 0:
+        print('Usage: \n   sketch_runner.py "sketch filename" [arguments for sketch] \n   Add'
+              ' "#!/path/to/sketch_runner.py" to sketches to make them executable')
+        return -2
+
+
+    return_val = -1
+    try:
+        print("Loading " + args[0])
+        runner = SketchRunner(sketch=args[0], default_library=True)
+        print("Running Sketch \n")
+        runner.run(args[1:])
+        return_val = 0
+    except FileNotFoundError:
+        print(args[0] + " not found \n")
+    except IsADirectoryError:
+        print(args[0] + " is a directory \n")
+    except AttributeError:
+        print("No \"loop()\" Function found. Exiting")
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt: Exiting")
+    return return_val
