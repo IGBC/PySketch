afe54bcdf5dbea9b78eb6978aae571cfc7bf2202
IGBC <fakespamtrap@gmail.com>
2016-05-22 23:52:56 +0200

    Added argument checking feature (whole feature)

--- pysketch/pysketch/__init__.py
+++ pysketch/pysketch/__init__.py
@@ -1,26 +1,51 @@
-from .runner import SketchRunner as SketchRunner
+from .runner import SketchRunner
+from .moduleloader import ModuleLoader
+from .arghelper import ArgChecker
 
 
-def interpreter(args):
-    if len(args) == 0:
-        print('Usage: \n   sketch_runner.py "__sketch filename" [arguments for __sketch] \n   Add'
-              ' "#!/path/to/sketch_runner.py" to sketches to make them executable')
-        return -2
+class Interpreter:
+    loader = None
+    checker = None
+    args = []
 
+    def __init__(self, args):
+        # Check a sketch is listed
+        if len(args) == 0:
+            print('Usage: \n   sketch_runner.py "__sketch filename" [arguments for __sketch] \n   Add'
+                  ' "#!/path/to/sketch_runner.py" to sketches to make them executable')
+            exit()
 
-    return_val = -1
-    try:
         print("Loading " + args[0])
-        runner = SketchRunner(sketch=args[0], default_library=True)
-        print("Running Sketch \n")
-        runner.run(args[1:])
-        return_val = 0
-    except FileNotFoundError:
-        print(args[0] + " not found \n")
-    except IsADirectoryError:
-        print(args[0] + " is a directory \n")
-    except AttributeError:
-        print("No \"loop()\" Function found. Exiting")
-    except KeyboardInterrupt:
-        print("Keyboard Interrupt: Exiting")
-    return return_val
+        self.__load_file(args[0])
+        print("Parsing Arguments")
+        self.__fix_args(args[1:])
+        self.__run()
+
+    def __load_file(self, file):
+        try:
+            self.loader = ModuleLoader(file)
+        except FileNotFoundError:
+            print(file + " not found \n")
+        except IsADirectoryError:
+            print(file + " is a directory \n")
+
+    def __fix_args(self, args):
+        assert isinstance(args, list)
+        self.checker = ArgChecker(self.loader.sketch)
+        self.args = args[:self.checker.max_args]
+        if not self.checker.verify_arg_count(self.args):
+            print("Insufficient Arguments Supplied: Exiting")
+            exit()
+
+    def __run(self):
+        return_val = -1
+        try:
+            runner = SketchRunner(sketch=self.loader.sketch, default_library=True)
+            print("Running Sketch \n")
+            runner.run(self.args)
+            return_val = 0
+        except AttributeError:
+            print("No \"loop()\" Function found: Exiting")
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt: Exiting")
+        return return_val
