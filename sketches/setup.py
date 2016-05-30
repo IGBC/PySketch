e045df8fec581b522d391f07e1bb7aed3c88377b
IGBC <fakespamtrap@gmail.com>
2016-05-30 10:57:07 +0200

    updated metadata

--- sketches/setup.py
+++ sketches/setup.py
@@ -3,16 +3,31 @@ from pypandoc import convert
 
 
 def read_md(file):
-    convert(file, 'rst')
+    text = convert(file, 'rst')
+    print("RST: " + text)
+    return text
+
 
 setup(
     name='sketches',
-    version='0.1',
+    version='0.1.0.1',
     packages=['sketches'],
+    scripts=['bin/pysketch'],
     url='https://github.com/IGBC/PySketch',
-    license='GPL V3.0',
-    author='SEGFAULT',
-    author_email='segfault@cbase.org',
-    description='python library for Arduino style sketches',
+    # license='GPL V3.0',
+    classifiers=['Development Status :: 2 - Pre-Alpha',
+                 'Environment :: Console',
+                 'Intended Audience :: End Users/Desktop',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Education',
+                 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                 'Natural Language :: English',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Topic :: Software Development :: Interpreters',
+                 'Topic :: Education', ],
+    author='IGBC',
+    author_email='segfault@c-base.org',
+    description='Write Arduino style sketches in Python',
     long_description=read_md("../README.md"),
 )
